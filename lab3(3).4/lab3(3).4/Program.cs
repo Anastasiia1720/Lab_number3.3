bc93196539d90aae63e73c5dbe28f28d461b0c4f
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-10-02 10:03:11 +0300

    commit

---
+++ lab3(3).4/lab3(3).4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 3, 8, 1, 4, 9, 2 };
+        (int min, int max) = FindMinMax(array);
+        Console.WriteLine($"Найменший елемент: {min}");
+        Console.WriteLine($"Найбільший елемент: {max}");
+    }
+
+    static (int, int) FindMinMax(int[] arr)
+    {
+        if (arr == null || arr.Length == 0)
+        {
+            throw new ArgumentException("Масив не може бути пустим.");
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        foreach (int num in arr)
+        {
+            if (num < min)
+            {
+                min = num;
+            }
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return (min, max);
+    }
+}
