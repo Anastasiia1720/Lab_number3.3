ce191f61f5e448fa51f3a4e646ade738b976d3b9
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-10-02 09:38:42 +0300

    commit

---
+++ lab3(3).2/lab3(3).2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть число:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine($"{number} є простим числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не є простим числом.");
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
